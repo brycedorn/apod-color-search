7017ec7c739652e182658d5c0762a771429b97e8
Bryce Dorn <bryce.dorn@uber.com>
2023-02-25 21:11:08 +0100

    rm node cache

--- api/routes/utils.ts
+++ api/routes/utils.ts
@@ -3,19 +3,10 @@ import { CacheResult, ClusterType, ColorType, SearchResult } from "./types.ts";
 
 const DISABLE_CACHE_GET = Deno.env.get("DISABLE_CACHE_GET");
 const DISABLE_CACHE_SET = Deno.env.get("DISABLE_CACHE_SET");
-const CACHE_URL = Deno.env.get("CACHE_URL");
-const USE_DENO_REDIS = Deno.env.get("USE_DENO_REDIS");
 
 export function SuccessResponse(data: SearchResult) {
   return new Response(JSON.stringify({ data }), {
     status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
-}
-
-export function ErrorResponse(error: Response) {
-  return new Response(JSON.stringify(error), {
-    status: 400,
     headers: { "Content-Type": "application/json" },
   });
 }
@@ -55,23 +46,18 @@ export function minimizeDistance(
   return getDistance(searchColor, colorA) - getDistance(searchColor, colorB);
 }
 
-export async function queryNodeCache(hex: string): Promise<CacheResult> {
+export async function queryCache(hex: string): Promise<CacheResult> {
   if (DISABLE_CACHE_GET) {
     return { isCached: false, result: [] };
   }
 
-  const response = await fetch(`${CACHE_URL}/get/${hex}`);
+  const result = await redis.get(hex);
 
-  if (!response.ok) {
-    throw Error();
-  }
-
-  const result = await response.json();
-
-  return result;
+  // @ts-expect-error: Day type
+  return { isCached: !!result, result: JSON.parse(result) };
 }
 
-export async function updateNodeCache(
+export async function updateCache(
   hex: string,
   value: string,
 ): Promise<void> {
@@ -79,39 +65,5 @@ export async function updateNodeCache(
     return;
   }
 
-  const response = await fetch(`${CACHE_URL}/set/${hex}`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: value,
-  });
-
-  if (!response.ok) {
-    throw Error();
-  }
-
-  await response.json();
-}
-
-export async function queryCache(hex: string): Promise<CacheResult> {
-  if (USE_DENO_REDIS) {
-    const result = await redis.get(hex);
-
-    // @ts-expect-error: Day type
-    return { isCached: !!result, result };
-  } else {
-    return await queryNodeCache(hex);
-  }
-}
-
-export async function updateCache(
-  hex: string,
-  value: string,
-): Promise<void> {
-  if (USE_DENO_REDIS) {
-    await redis.set(hex, value);
-  } else {
-    await updateNodeCache(hex, value);
-  }
+  await redis.set(hex, value);
 }
